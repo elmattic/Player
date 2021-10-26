fc8976c2ae15e714ef6ac2347b34d1717dd64568
elmattic <elmattic@users.noreply.github.com>
2021-10-26 09:46:01 +0200

    Add Rust submodule and use it

--- Player/ContentView.swift
+++ Player/ContentView.swift
@@ -3,7 +3,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        let result = rust_greeting("world")
+        let swift_result = String(cString: result!)
+        rust_greeting_free(UnsafeMutablePointer(mutating: result))
+        return Text(swift_result)
             .padding()
     }
 }
---
+++ Player/Coreplayer-Bridging-Header.h
@@ -0,0 +1,7 @@
+
+#ifndef Coreplayer_Bridging_Header_h
+#define Coreplayer_Bridging_Header_h
+
+#import "coreplayer.h"
+
+#endif /* Coreplayer_Bridging_Header_h */
